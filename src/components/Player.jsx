c51768412bf58e050ea2af248cd0177efc560b62
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2024-09-23 21:16:06 +0200

    🎉 Add : Application components and required styling files

---
+++ src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+   function handleEditClick() {
+        setIsEditing((editing)=> !editing);
+    };
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+        editAblePlayerName = event.target.value;
+    }
+
+    let editAblePlayerName = <span className="player-name">{playerName}</span>;
+    if (isEditing) {
+        editAblePlayerName = (
+            <input type="text" defaultValue={playerName} onChange={handleChange} />
+        );
+    }
+  return (
+    <li>
+      <span className="player">
+        {editAblePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? 'Save': 'Edit'}</button>
+    </li>
+  );
+}
