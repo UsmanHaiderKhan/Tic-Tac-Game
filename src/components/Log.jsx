8bc60b449d923ae18669be1f8be510e885c3a65f
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2024-09-30 18:34:34 +0200

    ♻️ Update : Log the result of Player Turns

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,3 +1,11 @@
-export default function Log() {
-    return (<p>Logs</p>)
-}
+export default function Log({turns}) {
+    return (
+        <ol id="log">
+            {turns.map((turn) => (
+                <li key={`${turn.square.col}${turn.square.row}`}>
+                    {turn.player} Selected {turn.square.row},{turn.square.col}
+                </li>
+            ))}
+        </ol>
+    )
+} 
